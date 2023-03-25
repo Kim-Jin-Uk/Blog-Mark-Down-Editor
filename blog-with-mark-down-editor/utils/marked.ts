7c9d9fa07316e10f839c8d156fe6648a51c531b6
김진욱 <76549646+Kim-Jin-Uk@users.noreply.github.com>
2023-03-25 15:25:04 +0900

    FEAT: 마크다운을 HTML로 변환하는 함수 추가

--- blog-with-mark-down-editor/utils/marked.ts
+++ blog-with-mark-down-editor/utils/marked.ts
@@ -1,6 +1,10 @@
-const convertMarkdownToStyle = (markdown: string) => {
+/**
+ * 마크다운을 (h, b, i, a, br) 스타일을 적용한 HTML로 변환하는 함수
+ * @param markdown 변환할 마크다운
+ * @returns 변환된 HTML
+ */
+const convertMarkdownToStyle = (markdown: string): string => {
   let html = markdown;
-
   // # 제목 태그 변환
   html = html.replace(/^#\s(.*)$/gm, '<h1>$1</h1>');
   // ## 제목 태그 변환
@@ -12,25 +16,35 @@ const convertMarkdownToStyle = (markdown: string) => {
   // 이탤릭체 변환
   html = html.replace(/_(.*?)_/g, '<i>$1</i>');
   // 링크 변환
-  html = html.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
+  html = html.replace(
+    /\[(.*?)\]\((.*?)\)/g,
+    '<a href="$2" target="_blank">$1</a>',
+  );
   // 줄바꿈 변환
   html = html.replace(/\n/g, '<br>');
   return html;
 };
-
-export const parseMarkdown = (markdown: string) => {
-  // 마크다운을 HTML로 변환하는 함수
-  const convertMarkdownToHtml = (markdown: string) => {};
-
-  // 마크다운을 줄바꿈으로 분리
-  const lines = markdown.split('\n');
-
-  let html = '';
-
-  // 분리된 줄마다 변환
-  for (let i = 0; i < lines.length; i++) {
-    html += convertMarkdownToHtml(lines[i]) + '\n';
+/**
+ * 마크다운을 HTML 형태로 변환하는 함수
+ * @param markdown 변환할 마크다운
+ * @param fns 변환에 사용할 함수 리스트
+ * @returns 변환된 HTML
+ */
+const convertMarkdownToHtml = (
+  markdown: string,
+  ...fns: ((markdown: string) => string)[]
+): string => {
+  let html = markdown;
+  for (const f of fns) {
+    html = f(html);
   }
-
   return html;
+};
+/**
+ * 마크다운을 HTML형태의 문자열로 파싱하는 함수
+ * @param {string} markdown 변환할 마크다운
+ * @returns {string} 변환된 HTML
+ */
+export const parseMarkdown = (markdown: string): string => {
+  return convertMarkdownToHtml(markdown, convertMarkdownToStyle);
 };
