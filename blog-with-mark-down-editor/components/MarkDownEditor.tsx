fb4a380b9b4a04db73b8d08da83be8efd0901e09
김진욱 <76549646+Kim-Jin-Uk@users.noreply.github.com>
2023-03-29 22:27:39 +0900

    REFACTOR: 마크다운 변환함수 가독성 개선     너무 길어진 코드를 기능별로 분리     일관된지 않은 코드 스타일의 일부 함수 코드 스타일 통일     리스트 형식 변환 함수 오류 수정 (ErrorCase: 직전 노드 대비 깊이가 2단계이상 얕아지며 같은 노드와 태그 형식이 다른 경우)     함수 설명 주석 추가

--- blog-with-mark-down-editor/components/MarkDownEditor.tsx
+++ blog-with-mark-down-editor/components/MarkDownEditor.tsx
@@ -1,5 +1,11 @@
+/**
+ * @copyright 김진욱
+ * @description 사용자의 입력중 마크다운 형식에 대해 그에 상응하는 HTML을 렌더링 합니다
+ * @created 23-03-26
+ * @updated 23-03-29
+ */
 import React, { useState } from 'react';
-import { parseMarkdown } from 'utils/marked';
+import { parseMarkdown } from 'utils/convertMarkdownToHTML';
 
 const MarkdownEditor = () => {
   const [markdown, setMarkdown] = useState('');
--- blog-with-mark-down-editor/utils/types.ts
+++
@@ -1,5 +0,0 @@
-export interface ListNode {
-  depth: number;
-  tag: string;
-  value: string;
-}
